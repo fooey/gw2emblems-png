3f63d6f268bb67b37fb8a5e65f44ac737d3e3617
Jason Rushton <github.com@schtuph.com>
2015-11-23 18:44:12 -0700

    trying to better match results to in game

--- render-test.babel.js
+++ render-test.babel.js
@@ -15,16 +15,26 @@ import {
 
 
 
-const celestial = {r: 211, g: 208, b: 207};
+const white = {r: 189, g: 186, b: 185};
+const red = {r: 135, g: 0, b: 10};
+const blueberry = {r: 36, g: 65, b: 122};
+const ancientSilver = {r: 157, g: 142, b: 108};
 const abyss = {r: 26, g: 24, b: 27};
+const green = {r: 28, g: 90, b: 45};
+const gray = {r: 72, g: 69, b: 70};
+const fuchsia = {r: 117, g: 25, b: 67};
+const oxblood = {r: 55, g: 4, b: 0};
+const orange = {r: 152, g: 63, b: 23};
+const salmon = {r: 133, g: 36, b: 26};
+const harvestGold = {r: 137, g: 85, b: 0};
+const lime = {r: 35, g: 80, b: 0};
 const blackCherry = {r: 128, g: 0, b: 0};
-const green = {r: 0, g: 128, b: 0};
 const blue = {r: 0, g: 0, b: 128};
 
 
 let testRange = [];
 
-for (let i = 220; i < 233; i++) {
+for (let i = 162; i < 170; i++) {
     testRange.push(i + 1);
 }
 
@@ -37,12 +47,12 @@ async.eachLimit(
 
         const fg = {
             id,
-            color1: blackCherry,
-            color2: abyss,
+            color1: lime,
+            color2: white,
         };
 
         const bg = {
-            id: 27,
+            id: 2,
             color: abyss,
         };
 
